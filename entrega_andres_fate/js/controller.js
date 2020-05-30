f7a5b71d56cb192f280ee5fa60892afb8aa0cec6
soypaduano <sebastian.paduano.m@gmail.com>
2020-05-30 18:48:04 +0200

    Nueva seccion de about us & funcionalidad de filtrado de personajes

--- entrega_andres_fate/js/controller.js
+++ entrega_andres_fate/js/controller.js
@@ -1,9 +1,21 @@
 function hideElement(type){
-    alert(type);
+    $('.servant-item').fadeOut(100);
+    var values = $("[type=" + type + "]").length;
+    if(values == 4){
+        $('.servants-list').removeClass('two').removeClass("four");
+        $('.servants-list').addClass('four');
+    } else if(values == 2) {
+        $('.servants-list').removeClass('two').removeClass("four");
+        $('.servants-list').addClass('two');
+    }
+
+    $("[type=" + type + "]").fadeIn(100);
 }
 
 $(document).ready(function(){
     $('.categoriesServants span').click(function(){
-       hideElement($(this).attr('type'))
+        $('.categoriesServants span').removeClass('selected');
+        $(this).addClass("selected");
+       hideElement($(this).attr('typeCat'))
     })
 });
