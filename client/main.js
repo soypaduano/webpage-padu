c33ad189d341970e8f734eed5044697f025d8f43
soypaduano <sebastian.paduano.m@gmail.com>
2020-11-06 00:46:03 +0100

    visualización de eventos + otras funcionalidades

--- client/main.js
+++ client/main.js
@@ -1,5 +1,11 @@
-var socket = io();
 
+let $oneDayEvent = $('.one-day-event').remove();
+let $weeklyEvent = $('.weekly-event').remove();
+$($oneDayEvent).hide();
+$($weeklyEvent).hide();
+
+
+/*var socket = io();
 var form = document.querySelector('form');
 
 form.addEventListener('submit', function(e) {
@@ -23,27 +29,105 @@ socket.on('message', function(text) {
   container.appendChild(seperator);
 
   container.scrollTop = container.scrollHeight;
-});
+});*/
+
+function readEventData(data){
+  //Evento de un día o evento de varios días?
+  let events = data['@graph'];
+  events.forEach(element => {
+
+    let $copy;
+    let recurrence = element['recurrence'];
+    if(recurrence){
+      $copy = $weeklyEvent.clone();
+    } else {
+      $copy = $oneDayEvent.clone();
+      let time = element['time'];
+      if(!time) time = 'Sin hora';
+      $($copy).find('.event-hour').text('hora: ' + time);
+    }
+    
+    let audience = element['audience'];
+
+    //District
+    let district = 'no-district'
+    if(element['address']) district = getDistrict(element['address']['district']['@id']);
+    $($copy).attr('district', district);
+
+
+    //Event title and description
+    $($copy).find('.event-title').text(element['title']); ;
+    let description = element['description'];
+    if(description === ''){
+      $($copy).find('.event-description').html('<p>Para ver la descripción, pincha aquí: <a id="link-to-event">+ info</a></p>')
+      $($copy).find('.event-more-info').remove();
+    } else {
+      $($copy).find('.event-description').text(description);
+    }
+
+    //id
+    $($copy).attr('id', element['id']);
+
+    let dateEnd = element['dtend'];
+    let dateStart = element['dtstart'];
+    if(!dateStart) dateStart = 'No hay fecha inicio'
+    //"2020-11-05 23:59:00.0"
+    $($copy).find('.event-day-start').text(dateStart)
+    
+    //event location
+    let eventLocation = element['event-location'];
+    if(!eventLocation){
+      eventLocation = 'Sin ubicación'
+      $($copy).find('.event-location').addClass('none');
+    } 
+    $($copy).find('.event-location').text(eventLocation);
+
+   
+    //Price
+    let free = element['free'];
+    if(free) price = 'Gratis'
+    else price = element['price'];
+    $($copy).find('.event-price').text(price);
+  
+
+    //Link
+    let link = element['link'];  
+    $($copy).find('#link-to-event').click(function () {
+      window.open(link);
+    });
+  
+    $('#events-list').append($copy);
+    $($copy).fadeIn(500);
+
+  });
+}
+
+//TODO: En eventos de varios días, quitar las horas de start / end. 
+
+
+function addOneDayEvent(){
+
+}
+
+function getDistrict(district){
+  var parts = district.split('/');
+  var lastSegment = parts.pop() || parts.pop();  // handle potential trailing slash
+  return lastSegment;
+}
 
 
 
 $.ajax({
-  url : 'http://127.0.0.1:5000/secret',
+  url : 'http://127.0.0.1:5000/datos',
   type : 'GET',
   dataType:'json',
   complete : function(req, data, error){
-    console.log(req);
-    console.log(data);
-    console.log(error);
     debugger;
   },
   success : function(data) {              
-      alert('Data: '+data);
-  },
-  error : function(request,error)
-  {
-    debugger;
-      alert("Request: "+JSON.stringify(request));
+    readEventData(data);
+  },error : function(request,error){
+
   }
 });
 
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
-
 var http = require('http');
-var server = http.Server(app);
+var request=require('request');
+var bodyParser = require('body-parser')
 
+
+var server = http.Server(app);
 app.use(express.static('client'));
+app.use(bodyParser.json())
 
 server.listen(PORT, function() {
   console.log('Chat server running');
@@ -21,4 +24,27 @@ io.on('connection', function(socket) {
   socket.on('message', function(msg) {
     io.emit('message', msg);
   });
+});
+
+
+app.all('/datos', function (reqq, ress, nextt) {
+
+
+var url_actividades_culturales_ocio_municipal_100_dias = "http://datos.madrid.es/egob/catalogo/206974-0-agenda-eventos-culturales-100.json"
+var url_agenda_actividades_culturales = "https://datos.madrid.es/egob/catalogo/300107-0-agenda-actividades-eventos.json"
+
+
+
+request({
+    url: url_actividades_culturales_ocio_municipal_100_dias,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      console.log(body);
+        ress.send(body);
+    }
+})
+
+
 });
