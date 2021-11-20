924053e3ca887a6ab05281c35d9a4b987c969528
sebastian.paduano.m@gmail.com <sebastian.paduano.m@gmail.com>
2021-11-20 23:08:59 +0100

    nuevos cambios web rothrigo

---
+++ web-rothrigo/js/main.js
@@ -0,0 +1,22 @@
+
+jQuery.curCSS = function (element, prop, val) {
+    return jQuery(element).css(prop, val);
+};
+
+var videoButton, musicButton, tourButton, shopButton, photoButton;
+
+function hideAll(){
+    
+}
+
+$(document).ready(function () {
+    $('.element-box a, .element-box img').hover(
+        function () {
+            var color = $(this).parent().attr('color-hover');
+            $("body").animate({ "background-color": color }, 200);
+            $(this).parent().find('.element-tag').show();
+        }, function () {
+            $("body").animate({ "background-color": "#FFF0C2" }, 200);
+            $(this).parent().find('.element-tag').hide();
+        });
+});
